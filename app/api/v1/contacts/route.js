6becb9e5bf5c3b7e22f69ebf060348be27cb1146
JFernandez0524 <njphonefix@gmail.com>
2024-03-22 21:11:38 -0400

    updated route

--- app/api/v1/contacts/route.js
+++ app/api/v1/contacts/route.js
@@ -1,6 +1,5 @@
 import { RESPONSE_LIMIT_DEFAULT } from 'next/dist/server/api-utils';
-const url =
-  'http://api.kvcore.com/v2/public/contacts?filter[hashtags][]=%23zillow';
+const url = 'http://api.kvcore.com/v2/public/contacts';
 
 export async function GET(request) {
   const res = await fetch(url, {
